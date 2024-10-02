2ee52c166914913cfe32d3c75ea6dd7ab9aabaf2
alif.meydi <alif.meydi@rds.co.id>
2024-10-02 15:33:23 +0700

    feat: first commit

---
+++ InterfaceSegregationPrinciple/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Human human = new Human();
+human.Eat();
+human.Work();
+
+Robot robot = new Robot();
+robot.Work();
+
+public interface IEatable
+{
+    void Eat();
+}
+
+public interface IWorkable
+{
+    void Work();
+}
+
+public class Human : IWorkable, IEatable
+{
+    public void Work()
+    {
+        Console.WriteLine("Kerja nih");
+    }
+
+    public void Eat() 
+    { 
+        Console.WriteLine("Makan nih"); 
+    }
+}
+
+public class Robot : IWorkable
+{
+    public void Work()
+    {
+        Console.WriteLine("Kerja nih");
+    }
+}
